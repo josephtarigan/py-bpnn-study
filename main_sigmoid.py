42a0749e3180d877eaf6255848f0f16bd80890cf
Joseph Tarigan <joseph.tarigan@gmail.com>
2017-09-24 21:15:49 +0700

    Add MSE plot

--- main_sigmoid.py
+++ main_sigmoid.py
@@ -3,12 +3,13 @@ import sigmoidCharsetDictionary as cd
 import cv2 as cv
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 inputNeuronCount = 108 + 1
 hiddenNeuronCount = 120 + 1
 outputNeuronCount = 36
 
-trainingCharsetsDir = 'trainingcharsets'
+trainingCharsetsDir = '~/Workspaces/pythonenv/tensorflow/py-bpnn-study/trainingcharsets'
 learningRate = 0.0450781
 momentum = 0.996094
 iteration = 1
@@ -39,10 +40,12 @@ while mse > targetMse :
     print ('Iteration : ' + str(iteration))
     for trainingSet in range (1, 10) :
         #print ('Charset ' + str(trainingSet))
+        epoch += 1
+        #print('Epoch : ' + str(epoch))
         # chars
         charIndex = 0;
-        for file in os.listdir(trainingCharsetsDir + '/' + str(trainingSet)) :
-            charImg = cv.imread(trainingCharsetsDir + '/' + str(trainingSet) + '/' + file, cv.IMREAD_GRAYSCALE)
+        for file in os.listdir(os.path.expanduser(trainingCharsetsDir + '/' + str(trainingSet) + '/')) :
+            charImg = cv.imread(os.path.expanduser(trainingCharsetsDir + '/' + str(trainingSet) + '/' + file), cv.IMREAD_GRAYSCALE)
             normalize_array(charImg, 255, 0)
         
             # feedforward
@@ -67,7 +70,8 @@ while mse > targetMse :
             
             weight2delta = (np.multiply(outputError, sigmoid_derivative(outputLayerOutput)))
             for i in range (0, outputNeuronCount) :
-                weight2[i] = np.add(weight2[i], learningRate * np.add(np.multiply(weight2delta[i], np.dot(weight2[i], outputLayerOutput[i])), (np.multiply(momentum, lastDelta2[i]))))
+                #np.dot(weight2[i], outputLayerOutput[i])
+                weight2[i] = np.add(weight2[i], learningRate * np.add(np.multiply(weight2delta[i], hiddenLayerOutput), (np.multiply(momentum, lastDelta2[i]))))
             #-------------------------- output layer -----------------------------
             
             #-------------------------- hidden layer -----------------------------
@@ -77,7 +81,8 @@ while mse > targetMse :
             #print(weight1delta)
             #input()
             for i in range (0, hiddenNeuronCount-1) :
-                weight1[i] = np.add(weight1[i], learningRate * np.add(np.multiply(weight1delta[i+1], np.dot(weight1[i], hiddenLayerOutput[i+1])), (np.multiply(momentum, lastDelta1[i+1]))))
+                #np.dot(weight1[i], hiddenLayerOutput[i+1])
+                weight1[i] = np.add(weight1[i], learningRate * np.add(np.multiply(weight1delta[i], inputLayerOutput), (np.multiply(momentum, lastDelta1[i+1]))))
             #-------------------------- hidden layer -----------------------------
             
             lastDelta1 = weight1delta
@@ -89,3 +94,9 @@ while mse > targetMse :
 # save weights
 np.savetxt("weight1.csv", weight1, delimiter=",")
 np.savetxt("weight2.csv", weight2, delimiter=",")
+
+# draw MSE graph
+plt.plot(mseList)
+plt.ylabel('MSE')
+plt.xlabel('Iteration')
+plt.show()
